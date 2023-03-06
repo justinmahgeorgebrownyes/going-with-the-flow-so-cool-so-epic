7aafbb63a1eaecde3920a6c4cc317e78a2ea3895
Justin Mah <91901581+justinmahgeorgebrownyes@users.noreply.github.com>
2023-03-06 16:46:47 -0500

    sort of shoots

---
+++ GameProduction2Testing/Assets/playerhi.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerhi : MonoBehaviour
+{
+    // Start is called before the first frame update
+        public float moveSpeed;
+        public Rigidbody2D rb;
+        public pizzahi weapon;
+
+        Vector2 moveDirection;
+        Vector2 mousePosition;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        if(Input.GetMouseButtonDown(0)) {
+
+            weapon.Fire();
+
+        }
+
+        moveDirection = new Vector2(moveX, moveY).normalized;
+        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+
+    }
+
+    private void FixedUpdate() {
+
+
+        rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+
+        Vector2 aimDirection = mousePosition - rb.position;
+        float aimAngle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = aimAngle;
+
+    }
+
+
+
+}
